74197d390ccd06d0a092204ba5094590dd7e6833
andres <andresxv@gmail.com>
2020-02-23 12:12:57 -0300

    AuthorType, relationships

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 Just me following some tutorials and experiments to test drive GraphQL. 
 
+### Tut 1, Example with Authors and Books in an Express Server
+
+https://www.youtube.com/watch?v=ZQL7tL2S0oQ
+https://github.com/WebDevSimplified/Learn-GraphQL/blob/master/server.js
--- server.js
+++ server.js
@@ -16,32 +16,67 @@ const app = express()
 const data = require('./data.js')
 //console.log(data)
 
-// Defining Custom Types
+// ------------ Defining Custom Types -----------------------
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	description: 'This represents a book, books have authors',
 	fields: () => ({
-		// No need to define a 'resolve' method in these (since making a Type, not a Query)
+		// No need to define a 'resolve' method in these first 3 
 		id: { type: GraphQLNonNull(GraphQLInt) }, 
 		name: { type: GraphQLNonNull(GraphQLString) },
-		authorId: { type: GraphQLNonNull(GraphQLInt) }
-	})
-})
-
-// Defining the queries we want available
-const RootQueryType = new GraphQLObjectType({
-	name: 'TheRootQuery',
-	description: 'This is the root query (top level)',
-	fields: () => ({
-		books: {
-			type: GraphQLList(BookType),
-			description: 'Alist of books',
-			resolve: () => data.books
+		authorId: { type: GraphQLNonNull(GraphQLInt) },
+		// This one is a relationship to another vertice, so it does need resolve method
+		author: { 
+			type: AuthorType,
+			resolve: (parentBook, args) => {
+				return data.authors.find(author=>author.id===parentBook.authorId)
+			}
 		}
 	})
 })
 
-// Defining the schema
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	description: 'Someone who worte a book',
+	fields: () => ({
+		id: {type:GraphQLNonNull(GraphQLInt)},
+		name: {type:GraphQLNonNull(GraphQLString)},
+		books: {
+			type: GraphQLList(BookType),
+			resolve: (thisAuthor) => {
+				return data.books.filter(book=>book.authorId===thisAuthor.id)
+			}
+		}
+	})
+
+})
+
+// ------------- Defining the queries we want available ----------------------
+const RootQueryType = new GraphQLObjectType({
+	name: 'TheRootQuery',
+	description: 'This is the root query (top level)',
+	fields: () => ({
+		book: {
+			type: BookType,
+			args: {
+				id: {type:GraphQLInt}
+			},
+			resolve: (parent, args) => data.books.find(book=>book.id===args.id)
+		},
+		books: {
+			type: GraphQLList(BookType),
+			description: 'A list of books',
+			resolve: () => data.books
+		},
+		authors: {
+			type: GraphQLList(AuthorType),
+			description: 'A list of authors',
+			resolve: () => data.authors
+		}
+	})
+})
+
+// ------------ Defining the schema ----------------
 const schema = new GraphQLSchema({
 	query: RootQueryType
 });
