716298bc1e979a908151cf011d859956025321ee
andres <andresxv@gmail.com>
2020-02-23 10:33:21 -0300

    data file, book type, and not so basic schema

--- server.js
+++ server.js
@@ -1,24 +1,55 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
-const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql') 
+const {
+	GraphQLSchema, 
+	// GraphQLObjectType is Used for BOTH: declaring custom types (like "Book") 
+	// AND declaring the Root Query:
+	GraphQLObjectType,  
+	GraphQLNonNull,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLList
+} = require('graphql') 
 
 const app = express()
 
-const schema = new GraphQLSchema({
-	query: new GraphQLObjectType({
-		name: 'Hellou', // no puede contener espacios
-		fields: () => ({
-			dummyMessage:{
-				type: GraphQLString,
-				resolve: () => 'Hola, buenas! que tal?'
-			}
-		})
+const data = require('./data.js')
+//console.log(data)
+
+// Defining Custom Types
+const BookType = new GraphQLObjectType({
+	name: 'Book',
+	description: 'This represents a book, books have authors',
+	fields: () => ({
+		// No need to define a 'resolve' method in these (since making a Type, not a Query)
+		id: { type: GraphQLNonNull(GraphQLInt) }, 
+		name: { type: GraphQLNonNull(GraphQLString) },
+		authorId: { type: GraphQLNonNull(GraphQLInt) }
 	})
 })
 
+// Defining the queries we want available
+const RootQueryType = new GraphQLObjectType({
+	name: 'TheRootQuery',
+	description: 'This is the root query (top level)',
+	fields: () => ({
+		books: {
+			type: GraphQLList(BookType),
+			description: 'Alist of books',
+			resolve: () => data.books
+		}
+	})
+})
+
+// Defining the schema
+const schema = new GraphQLSchema({
+	query: RootQueryType
+});
+
+// Plug to express
 app.use('/graphql', expressGraphQL({
 	schema: schema,
 	graphiql: true
 }))
 
-app.listen(8051, (req, res)=> console.log('Rannninga'))
+app.listen(8052, (req, res) => console.log('Rannninga'))
